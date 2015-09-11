810db3456d4aca7504a63e9ec94c03945524ea8b
thcmatias <thcmatias@gmail.com>
2015-09-11 08:59:33 -0300

    * PlanilhaController arrumadinho, cheirosinho e limpinho agora, com comentarios (!!!)

--- src/controller/PlanilhaController.java
+++ src/controller/PlanilhaController.java
@@ -8,24 +8,56 @@ import java.io.IOException;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class PlanilhaController {
+	/**
+	 * A criacao das pastas comeca sua contagem por 1. Caso queira redefinir por
+	 * onde deve comecar a contagem das Pastas, mude o valor desta constante
+	 */
+	private final static int indice0 = 1;
+	/**
+	 * Planilha com a lista de cartoes de credito e categorias.
+	 */
 	public static XSSFWorkbook arq_base = carregaPlanilhaBase();
+	/**
+	 * Planilha onde estao sendo cadastradas as transacoes. Em varios outros
+	 * momentos vamos nos referir a ela como "Pasta" ou "pasta", pois seus nome
+	 * pode variar (Pastai.xlsx, onde i = 1, 2, 3, 4, ...)
+	 */
 	public static XSSFWorkbook arq_pasta = carregaPasta();
+	/**
+	 * Nome da Pasta atualmente usada.
+	 */
 	static String nome_arq_pasta = descobreNome();
 
+	/**
+	 * Monta uma String com um nome para uma Pasta.
+	 */
 	public static String nomeMaker(int i) {
 		return "Pasta" + i + ".xlsx";
 	}
 
+	/**
+	 * Carrega o arquivo xlsx da Pasta a ser utilizada. Caso nenhum arquivo
+	 * Pasta exista ou o atual tenha 200 ou mais cadastros de transacoes, cria
+	 * uma nova Pasta para evitar que o arquivo fique muito grande e nao seja
+	 * mais possivel acessa-lo.
+	 */
 	private static XSSFWorkbook carregaPasta() {
-		int i = 1;
-		String nomeArq = null;
-		nomeArq = nomeMaker(i);
+		int i = indice0;
+		String nomeArq = nomeMaker(i);
 		File arquivo = new File(nomeArq);
 		XSSFWorkbook arq_xlsx = null;
-
+		/**
+		 * Enquanto existirem arquivos pasta para abrir devemos testah-los a fim
+		 * de saber se ele jah tem 200 ou mais cadastros de transacoes
+		 */
 		while (arquivo.exists()) {
 			arq_xlsx = abrePlanilha(arquivo);
+
 			if (arq_xlsx.getSheetAt(0).getPhysicalNumberOfRows() > 201) {
+				/**
+				 * Caso a pasta aberta tenha muitos cadastros de transacoes,
+				 * devemos tentar abrir a proxima Pastai.xlsx
+				 */
 				i++;
 				nomeArq = nomeMaker(i);
 				arquivo = new File(nomeArq);
@@ -35,18 +67,31 @@ public class PlanilhaController {
 					e.printStackTrace();
 				}
 			} else {
+				/**
+				 * Caso a pasta aberta nao tenha tantos cadastros de transacoes
+				 * assim, utilizamos ela mesma e podemos, portanto, sair do loop
+				 */
 				break;
 			}
 		}
+		/**
+		 * Se sairmos do loop sem conseguir abrir nenhuma Pasta, pois todas
+		 * estao cheias ou por nao existir nenhuma, criamos uma nova Pasta.
+		 */
 		if (!arquivo.exists()) {
 			arq_xlsx = criaPlanilha(arquivo);
 		}
 
 		return arq_xlsx;
 	}
-	
+
+	/**
+	 * Como o metodo para abrir a Pasta retorna o proprio arquivo xlsx, pode ser
+	 * que em algum dado momento precisemos saber qual o nome da pasta atual.
+	 * Este metodo descobre o nome da Pasta que estah sendo usada atualmente
+	 */
 	private static String descobreNome() {
-		int i = 1;
+		int i = indice0;
 		String nomeArq = nomeMaker(i);
 		File arquivo = new File(nomeMaker(i));
 
@@ -55,10 +100,14 @@ public class PlanilhaController {
 			i++;
 			arquivo = new File(nomeMaker(i));
 		}
-		
+
 		return nomeArq;
 	}
 
+	/**
+	 * Cria um novo arquivo xlsx com uma pagina de planilha nomeada "conta". (ou
+	 * seja, serve apenas para a criacao de planilhas Pastas)
+	 */
 	public static XSSFWorkbook criaPlanilha(File arquivo) {
 		XSSFWorkbook arq_xlsx = new XSSFWorkbook();
 		arq_xlsx.createSheet("conta");
@@ -70,6 +119,9 @@ public class PlanilhaController {
 		return arq_xlsx;
 	}
 
+	/**
+	 * Abre um arquivo xlsx existente.
+	 */
 	public static XSSFWorkbook abrePlanilha(File arquivo) {
 		XSSFWorkbook arq_xlsx = null;
 		try {
@@ -80,18 +132,29 @@ public class PlanilhaController {
 		return arq_xlsx;
 	}
 
+	/**
+	 * Carrega a planilha base com os cartoes e categorias cadastrados. O nome
+	 * da planilha deve ser "listas desp.xlsx".
+	 */
 	private static XSSFWorkbook carregaPlanilhaBase() {
 		return abrePlanilha(new File("listas desp.xlsx"));
 	}
 
-	public static void fechaPlanilha() {
+	/**
+	 * Descarrega todas as planilhas da memoria
+	 */
+	public static void fechaPlanilhas() {
 		try {
 			arq_base.close();
+			arq_pasta.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Salva as alteracoes feitas na base.
+	 */
 	public static void salvarBase() {
 		try {
 			arq_base.write(new FileOutputStream(new File("listas desp.xlsx")));
@@ -100,6 +163,9 @@ public class PlanilhaController {
 		}
 	}
 
+	/**
+	 * Salva as alteracoes feitas na Pasta.
+	 */
 	public static void salvarPasta() {
 		try {
 			arq_pasta.write(new FileOutputStream(new File(nome_arq_pasta)));
@@ -108,8 +174,8 @@ public class PlanilhaController {
 		}
 	}
 
-	public static void main(String[] args) {
-		System.out.println(arq_pasta.getSheetAt(0).getRow(0));
-		System.out.println(arq_pasta.getSheetAt(0).getRow(0).getCell(4));
-	}
+	// public static void main(String[] args) {
+	// System.out.println(arq_pasta.getSheetAt(0).getRow(0));
+	// System.out.println(arq_pasta.getSheetAt(0).getRow(0).getCell(4));
+	// }
 }
